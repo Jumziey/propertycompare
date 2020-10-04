57ccd6ee08f1c0f815624a5163ebb41e447e7780
Jesper 'jumziey' Vesterberg <jumziey@gmail.com>
2020-10-04 12:32:02 +0200

    Added definitions for validating that both a city and streetaddress uniquely identifies a property.

--- pkg/propertydb/validate.go
+++ pkg/propertydb/validate.go
@@ -9,7 +9,15 @@ func ValidateAddAndShow(t *testing.T) {
 func ValidateAddSameTwiceError(t *testing.T) {
 	//Add valid property twice, make sure an error Shows on second add
 }
-func ValidateUpdateCity(t *testing.T) {
+
+func ValidateCityAndStreetAddressIdentifies(t *testing.T) {
+	//Add two valid properties in the same city but different addresses
+	//make sure no error
+	//Add two valid properties on the same address but different cities
+	//make sure no error
+}
+
+func ValidateUpdate(t *testing.T) {
 	//Add property, update property, validate update took place with Show
 }
 
