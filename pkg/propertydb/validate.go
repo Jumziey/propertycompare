b7235e24d9912576a1f28dac306abe4b5650ebea
Jesper 'jumziey' Vesterberg <jumziey@gmail.com>
2020-10-04 12:55:19 +0200

    Added missing function parameter `PropertyDB` on all validate functions

--- pkg/propertydb/validate.go
+++ pkg/propertydb/validate.go
@@ -2,29 +2,29 @@ package propertydb
 
 import "testing"
 
-func ValidateAddAndShow(t *testing.T) {
+func ValidateAddAndShow(t *testing.T, pdb PropertyDB) {
 	//Add a valid property, make sure no error
 	//Check city on Show
 }
-func ValidateAddSameTwiceError(t *testing.T) {
+func ValidateAddSameTwiceError(t *testing.T, pdb PropertyDB) {
 	//Add valid property twice, make sure an error Shows on second add
 }
 
-func ValidateCityAndStreetAddressIdentifies(t *testing.T) {
+func ValidateCityAndStreetAddressIdentifies(t *testing.T, pdb PropertyDB) {
 	//Add two valid properties in the same city but different addresses
 	//make sure no error
 	//Add two valid properties on the same address but different cities
 	//make sure no error
 }
 
-func ValidateUpdate(t *testing.T) {
+func ValidateUpdate(t *testing.T, pdb PropertyDB) {
 	//Add property, update property, validate update took place with Show
 }
 
-func ValidateList(t *testing.T) {
+func ValidateList(t *testing.T, pdb PropertyDB) {
 	//Add three properties, validate same with List()
 }
 
-func ValidateDelete(t *testing.T) {
+func ValidateDelete(t *testing.T, pdb PropertyDB) {
 	//Add three properties, delete one validate with List()
 }
