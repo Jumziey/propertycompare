bbc2af1a8ce2cd4829b9ab4e4ab7b434fa764232
Jesper 'jumziey' Vesterberg <jumziey@gmail.com>
2020-10-04 13:47:11 +0200

    Removed returning info on the PropertyDB interface

--- pkg/propertydb/interface.go
+++ pkg/propertydb/interface.go
@@ -1,9 +1,9 @@
 package propertydb
 
 type PropertyDB interface {
-	Add(City, StreetAddress, Info) (Info, error) //more
-	Delete(City, StreetAddress) (Info, error)
-	Update(City, StreetAddress, Info) (Info, error)
+	Add(City, StreetAddress, Info) error //more
+	Delete(City, StreetAddress) error
+	Update(City, StreetAddress, Info) error
 	List() ([]Listing, error) //more
-	Show(City, StreetAddress) (Info, error)
+	Show(City, StreetAddress) (Listing, error)
 }
